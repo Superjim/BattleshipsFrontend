1b4165b525dc9113267466145f11074e95fe3b8a
Jim Jenkinson <jamesjenkinson10@gmail.com>
2023-08-03 01:05:50 +0100

    Created player authentication context, can change player for dev testing

--- battleships-frontend/src/App.tsx
+++ battleships-frontend/src/App.tsx
@@ -1,16 +1,17 @@
-import "./App.css";
+import React from "react";
+import { AuthProvider } from "./contexts/AuthContext";
 import NavBar from "./components/NavBar";
 import PlayerList from "./components/PlayerList";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <NavBar />
-      <div className="container-fluid">
+    <AuthProvider>
+      <div className="App">
+        <NavBar />
         <p>Battleships</p>
         <PlayerList />
       </div>
-    </div>
+    </AuthProvider>
   );
 };
 
